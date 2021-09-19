d313fcb3f0cc5ca379cd1821c5447c410ce7f1d6
imaldi <aldiirsanmajid@gmail.com>
2021-09-20 02:27:49 +0700

    sesudah di edit mas farhan

--- lib/data_layer/repos/login_repo.dart
+++ lib/data_layer/repos/login_repo.dart
@@ -1,60 +1,64 @@
+import 'dart:collection';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:anipay_flutter/data_layer/consts/string_consts.dart';
-import 'package:anipay_flutter/data_layer/model/plain_login_request_model.dart';
-import 'package:anipay_flutter/data_layer/model/plain_login_response_model.dart';
 import 'package:anipay_flutter/data_layer/model/request_body.dart';
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class LoginRepo {
+  Dio _dio = Dio();
+  ApiService() {
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+      return client;
+    };
+  }
+
   // Future<RequestBody<PlainLoginRequestModel>>loginToServer(String phone, String password) async {
-  Future<ResponseBody?>loginToServer(RequestBody requestTrx) async {
+  Future<Response?> loginToServer(RequestBody requestTrx) async {
     String baseUrl = "${BASE_URL + EA_MOBILES}";
     print("baseUrl: ${baseUrl}");
-    print("request json: ${requestTrx.toJson()}");
+    print("request json: ${requestTrx.toString()}");
+
+    Map<String, String> headers = new HashMap();
+    headers["content-type"] = "application/json";
+    headers["versionCode"] = "1";
+    print("headers: ${headers}");
 
     try {
-      var response = await http.post(Uri.parse(baseUrl), body:
-      // requestTrx,
-      {
-        'data': requestTrx.data,
-        'method': requestTrx.method,
-        'versioncode': requestTrx.versioncode,
-        'sessionid': requestTrx.sessionid
-      },
-      // {
-      //   'phone': phone,
-      //   'password': password,
-      //   // 'device_name': device
-      // },
-          headers: {
-        'Accept': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded'
-      });
+      // var response = await http.post(Uri.parse(baseUrl),
+      //   body: jsonEncode(requestTrx), headers: headers
+      // );
+      Response response = await _dio.post(baseUrl, options: Options(headers: headers), data: jsonEncode(requestTrx));
 
+      print(response.statusCode);
+      print(response.data);
       // Map<String, dynamic> jsonResponse = json.decode(response.body);
       if (response.statusCode == 200) {
-        final Map<String, dynamic> data = jsonDecode(response.body);
-        // json.decode(response.body);
-        final ResponseBody? dataAuth = ResponseBody.fromJson(data);
-        // String token = dataAuth?.token ?? "";
-        // String tokenType = dataAuth?.tokenType ?? "";
-        // int expiresIn = dataAuth?.expiresIn ?? 0;
-        print(response.statusCode);
-        print(data);
-        print("dataAuth: ${dataAuth.toString()}");
-        print(response);
-        // if (expiresIn > 0) {
-        //   saveDataPref(token, tokenType, expiresIn);
-        // }
-        // return AuthResponse.fromJson(data);
-        return dataAuth;
+        // final Map<String, dynamic> data = jsonDecode(response.body);
+        // // json.decode(response.body);
+        // final ResponseBody? dataAuth = ResponseBody.fromJson(data);
+        // // String token = dataAuth?.token ?? "";
+        // // String tokenType = dataAuth?.tokenType ?? "";
+        // // int expiresIn = dataAuth?.expiresIn ?? 0;
+        // print(response.statusCode);
+        // print(data);
+        // print("dataAuth: ${dataAuth.toString()}");
+        // print(response);
+        // // if (expiresIn > 0) {
+        // //   saveDataPref(token, tokenType, expiresIn);
+        // // }
+        // // return AuthResponse.fromJson(data);
+        // return dataAuth;
       } else {
-        print("response.body ${response.body}");
-
-        // return response.body;
+        // print("response.body ${response.body}");
         //
-        throw Exception();
+        // // return response.body;
+        // //
+        // throw Exception();
       }
       // return response.statusCode;
     } catch (e) {
